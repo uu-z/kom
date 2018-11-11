ea3bda4766545afd8889019e30a78d16dbd5dde9
yuyuko <ncysatnaf@gmail.com>
2018-11-11 15:11:51 +0800

    fix

--- routes/axios.js
+++ routes/axios.js
@@ -2,8 +2,7 @@ const axios = require('axios');
 
 module.exports = {
 	'post /axios': async (ctx, next) => {
-		const { url } = ctx.request.query;
-		let { data } = await axios.get(url);
+		let { data } = await axios(ctx.request.body);
 		ctx.body = data;
 	}
 };
