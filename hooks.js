510e1c7f77cde1c9863a9bfbd9b828a8214599ed
yuyuko <ncysatnaf@gmail.com>
2018-11-13 17:18:48 +0800

    update hook

--- hooks.js
+++ hooks.js
@@ -1,49 +1,47 @@
-const Koa = require('koa');
-const Router = require('koa-router');
-const _ = require('lodash');
-const requireDir = require('require-dir');
-const Mhr = require('menhera').default;
+const Koa = require("koa");
+const Router = require("koa-router");
+const _ = require("lodash");
+const requireDir = require("require-dir");
+const Mhr = require("menhera").default;
 
 const app = new Koa();
 const router = new Router();
-const modules = requireDir('./modules');
+const modules = requireDir("./modules");
 
 Mhr.$use({
-	$use: {
-		$({ _val }) {
-			app.use(_val);
-		}
-	},
-	$hooks: {
-		$({ _val }) {
-			if (typeof _val == 'function') {
-				_val();
-			}
-		}
-	},
-	$routes: {
-		_({ _val }) {
-			_.each(_val, (val, key) => {
-				val = Array.isArray(val) ? val : [ val ];
-				const [ method, path ] = key.split(' ');
-				router[method](path, ...val);
-			});
-		}
-	},
-	$config: {
-		_({ _, _val }) {
-			const { PORT } = _val;
-			_.$use({
-				use: [ router.routes(), router.allowedMethods() ]
-			});
-			app.listen(PORT);
-			console.info(`Server listening on port ${PORT}...`);
-		}
-	}
+  $use: {
+    $({ _val }) {
+      app.use(_val);
+    }
+  },
+  $hooks: {
+    $({ _val }) {
+      if (typeof _val == "function") {
+        _val();
+      }
+    }
+  },
+  $routes: {
+    $({ _key, _val }) {
+      _val = Array.isArray(_val) ? _val : [_val];
+      const [method, path] = _key.split(" ");
+      router[method](path, ..._val);
+    }
+  },
+  $config: {
+    _({ _, _val }) {
+      const { PORT } = _val;
+      _.$use({
+        use: [router.routes(), router.allowedMethods()]
+      });
+      app.listen(PORT);
+      console.info(`Server listening on port ${PORT}...`);
+    }
+  }
 });
 
 _.each(modules, val => {
-	Mhr.$use(val);
+  Mhr.$use(val);
 });
 
 module.exports = Mhr;
